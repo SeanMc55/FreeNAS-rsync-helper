c571003a78a76ddfd1d669ebc7feb31e3c19f8e7
SeanMc55 <seanmcmillin3259@gmail.com>
2020-06-06 16:46:10 -0700

    Update rsync-tool.py
    Added in quick link selection and option to send just files or entire directory

--- rsync-tool.py
+++ rsync-tool.py
@@ -4,6 +4,13 @@ os.system("clear")
 initial_dir = "/mnt/"
 # initial_dir = "/Volumes/Data/"
 
+quick_list = [\
+    "/mnt/fleeting_files/Transmission_Downloads/reencodes/render_h264_to_HEVC_q20/",\
+    "",\
+    "",\
+    ""\
+        ]
+
 # find first dir to copy from
 def get_copy_dir(initial_dir):
     copy_dir = [initial_dir]
@@ -36,7 +43,14 @@ def get_copy_dir(initial_dir):
         print('')
         os.chdir(new_dir)
 
-    copy_dir = copy_dir[:-1]
+    print('')
+    print("Send")
+    print(" 1) the directory")
+    print(" 2) the files")
+    choice = input(": ")
+    if choice == "2":
+        copy_dir = copy_dir[:-1]
+
     for entry in copy_dir:
         new_dir = new_dir + entry
     return new_dir
@@ -76,14 +90,30 @@ def get_paste_dir(initial_dir):
         new_dir = new_dir + entry
     return new_dir
 
+# get the directories to copy from
 os.chdir(initial_dir)
 copydir = '"'+get_copy_dir(initial_dir)+'"'
-os.chdir(initial_dir)
-pastedir = '"'+get_paste_dir(initial_dir)+'"'
-os.chdir(initial_dir)
 
 os.system("clear")
 
+#pick from quick list or choose directory
+choice = input("Do you want to use a quick destination? (y/n): ")
+if choice == 'y':
+    index = 1
+    for i in quick_list:
+        print("{} {}".format(index, i))
+        index += 1
+    print('')
+    selection = input("Which quick destination would you like to use?: ")
+    selection = int(selection)-1
+    pastedir = quick_list[selection]
+    pastedir = '"'+pastedir+'"'
+else:
+    # get the directories to copy to
+    os.chdir(initial_dir)
+    pastedir = '"'+get_paste_dir(initial_dir)+'"'
+
+os.system("clear")
 command = "rsync -rv --progress "+copydir+' '+pastedir
 print(command)
 os.system(command)
